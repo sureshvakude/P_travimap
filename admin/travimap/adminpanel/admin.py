2c8b741c236e8a07ef84139f6b7e1330f7f4e14c
suresh <sureshvakude8906@gmail.com>
2025-02-17 01:22:23 +0530

    all models added in admin - 20250217C

--- admin/travimap/adminpanel/admin.py
+++ admin/travimap/adminpanel/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from django.utils.html import format_html
+from .models import User, Place, Post, Trip
 
-# Register your models here.
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email', 'mobile_number', 'gender', 'role', 'profile_picture_preview')
+    
+    def profile_picture_preview(self, obj):
+        return format_html(f'<img src="{obj.profile_picture.url}" width="50" height="50" style="border-radius:50%;" />')
+
+class PlaceAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', 'region', 'rating', 'image_preview')
+
+    def image_preview(self, obj):
+        return format_html(f'<img src="{obj.img.url}" width="100" height="100" />')
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('name', 'caption', 'like', 'image_preview')
+
+    def image_preview(self, obj):
+        return format_html(f'<img src="{obj.img.url}" width="100" height="100" />')
+
+class TripAdmin(admin.ModelAdmin):
+    list_display = ('name', 'destination', 'budget', 'type', 'image_preview')
+
+    def image_preview(self, obj):
+        return format_html(f'<img src="{obj.img.url}" width="100" height="100" />')
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Place, PlaceAdmin)
+admin.site.register(Post, PostAdmin)
+admin.site.register(Trip, TripAdmin)
--- admin/travimap/travimap/settings.py
+++ admin/travimap/travimap/settings.py
@@ -112,3 +112,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
+# Only allow superusers to access admin panel
+LOGIN_URL = '/admin/login/'
