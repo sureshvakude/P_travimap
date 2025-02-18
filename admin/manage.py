c1059c1930a43bb41da8737762380adeb9c80873
suresh <sureshvakude8906@gmail.com>
2025-02-19 00:48:54 +0530

    updated admin panel - 20250219V

--- admin/manage.py
+++ admin/manage.py
@@ -3,7 +3,6 @@
 import os
 import sys
 
-
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'admin.settings')
@@ -15,8 +14,9 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
+    if len(sys.argv) == 2:
+        sys.argv.append("3002")
     execute_from_command_line(sys.argv)
-
 
 if __name__ == '__main__':
     main()
